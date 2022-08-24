9e7828e96a6d934b2eb769cccd486de13f6114ee
Kracon3 <Kracon7136@gmail.com>
2022-08-24 12:17:25 -0600

    added gitignore file

---
+++ ColorChange/ColorChangeView.swift
@@ -0,0 +1,25 @@
+//
+//  ColorChangeView.swift
+//  ColorChange
+//
+//  Created by Beckstead, Kayden on 8/24/22.
+//
+
+import SwiftUI
+
+struct ColorChangeView: View
+{
+    var body: some View
+    {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ColorChangeView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        ColorChangeView()
+    }
+}
---
+++ ColorChange/Resources/ColorChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangeApp.swift
+//  ColorChange
+//
+//  Created by Beckstead, Kayden on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ColorChangeView()
+        }
+    }
+}
